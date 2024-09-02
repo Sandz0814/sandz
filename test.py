c65dcdb1765f5ede15f79af6a1ceacd7cda4d6a7
Sandz0814 <sandrojimena0814@gmail.com>
2024-09-02 22:12:25 +0800

    add route

--- test.py
+++ test.py
@@ -7,9 +7,9 @@ driver = webdriver.Chrome()
 driver.implicitly_wait(10)
 driver.maximize_window()
 
-driver.get('https://sandz0814.github.io/Tribu-Sinag-Padayon.com/')
+driver.get('https://sandz0814.github.io/sandz/Contact.html')
 
-x = driver.find_element(By.CSS_SELECTOR, "body header footer p")
+x = driver.find_element(By.CSS_SELECTOR, "tbody tr:nth-child(2) td:nth-child(2)")
 print(x.text)
 
 time.sleep(10)
